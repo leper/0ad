c382843307a63e1eb8bdac498c5af454f6725457
leper <leper@wildfiregames.com>
2015-12-12 08:21:39 +0100

    Fix broken version detection.
    https://bugzilla.mozilla.org/show_bug.cgi?id=1145882

--- libraries/source/spidermonkey/patch.sh
+++ libraries/source/spidermonkey/patch.sh
@@ -2,3 +2,7 @@
 # Apply patches if needed
 # This script gets called from build-osx-libs.sh and build.sh.
 
+# Fix the version specification code which used PYTHON before it was set.
+# The second patch is required to not add autoconf as a dependency.
+patch -p1 < ../FixVersionDetection.diff
+patch -p1 < ../FixVersionDetectionConfigure.diff
